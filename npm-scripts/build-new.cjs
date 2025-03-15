c3785a597a55db2325806a19b627f2cfee28c9a3
xclhove <2959169303@qq.com>
2025-03-16 01:40:23 +0800

    fix: 修复自动生成的版本号日期错误的bug

--- npm-scripts/build-new.cjs
+++ npm-scripts/build-new.cjs
@@ -6,7 +6,7 @@ const getVersion = () => {
   const now = new Date()
   const year = now.getFullYear()
   const month = (now.getMonth() + 1).toString().padStart(2, '0')
-  const day = now.getDay().toString().padStart(2, '0')
+  const day = now.getDate().toString().padStart(2, '0')
   const hour = now.getHours().toString().padStart(2, '0')
   const minute = now.getMinutes().toString().padStart(2, '0')
   const second = now.getSeconds().toString().padStart(2, '0')
